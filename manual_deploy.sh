b5fc5606c1d6a22f88532d80dcad820c8411c150
Ashley Craig <ashleycraig19872@hotmail.co.uk>
2018-04-22 13:12:10 +0100

    Update manual_deploy shell script

--- manual_deploy.sh
+++ manual_deploy.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 echo branch you want to deploy is $1
+branch=$1
+
+#put in a check that the branch we are on is the same as $1 and that it's not master or develop
+
+#check this is the best way to check the branch we are on, can use grep
+echo $(git rev-parse --abbrev-ref HEAD)
+
 echo environment you want to deploy to is $2
+environment=$2
+
+#modifiy the deployment json file
+
+#maybe put the add and commit commands into this script
+
+#push the branch
+#git push origin $1
